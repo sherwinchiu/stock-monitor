1ebddd1093337e6c6fc1ee5d1bead9be8eb63e7e
sherwinc <sherwin.chiu89@gmail.com>
2021-02-18 17:42:56 -0500

    many bug fixes

--- client/client.ino
+++ client/client.ino
@@ -1,5 +1,6 @@
 #include <LiquidCrystal.h>
-zz
+#include <Servo.h>
+
 #define     rs      12
 #define     en      11
 #define     d4      5
@@ -14,12 +15,16 @@ double price;
 int priceDecimal;
 double closePrice;
 int closeDecimal;
-byte count = 0;
+int count = 0;
+int debounceDelay = 500;
+int lastDebounceTime = millis();
+Servo servo1;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 void setup() {
     Serial.begin(9600);  
     pinMode(inPin1, INPUT);
     pinMode(inPin2, INPUT);
+    servo1.attach(servo);
     lcd.begin(16, 2);
 }
     
@@ -29,29 +34,41 @@ void loop() {
     chooseTicker();
 }
 void changeTicker(){
-    if (digitalRead(inPin1)){
-        count++;
-        if(count == 9)
-            count =0;
-        Serial.write('N');
-    } else if (digitalRead(inPin2)){
-        count--;
-        if (count == -1)
-            count = 8;
-        Serial.write('B');
+    if(millis() - lastDebounceTime > debounceDelay){
+      if (digitalRead(inPin1)){
+          count++;
+          if(count >= 9)
+              count =0;
+          Serial.write(0);
+          lastDebounceTime = millis();
+      } else if (digitalRead(inPin2)){
+          count--;
+          if (count <= -1)
+              count = 8;
+          Serial.write(1);
+          lastDebounceTime = millis();
+      }
     }
 }
 void chooseTicker(){
-    switch(count){
-        case 0: standardPrint("GME");
-        case 1: standardPrint("AMC");
-        case 2: standardPrint("BBBY");
-        case 3: standardPrint("NOK");
-        case 4: standardPrint("BB");
-        case 5: standardPrint("TSLA");
-        case 6: standardPrint("AAPL");
-        case 7: standardPrint("DIS");
-        case 8: standardPrint("NVDA");
+    if(count ==0){
+        standardPrint("GME");
+    } else if (count == 1){
+        standardPrint("AMC");
+    } else if (count == 2){
+        standardPrint("BBBY");
+    } else if (count == 3){
+        standardPrint("NOK");
+    } else if (count == 4){
+        standardPrint("BB");
+    } else if (count == 5){
+        standardPrint("TSLA");
+    } else if (count == 6){
+        standardPrint("AAPL");
+    } else if (count == 7){
+        standardPrint("DIS");
+    } else if (count == 8){
+        standardPrint("NVDA");
     }
 }
 void getPrices(){
@@ -62,13 +79,18 @@ void getPrices(){
       closePrice = Serial.read();
       closeDecimal = Serial.read();
       closePrice += closeDecimal/100.0;
+      if(calcChange() < 0)
+        servo1.write(45);
+      else if (calcChange() > 0)
+        servo1.write(-45);
     }
+    
 }
 double calcChange(){
     return (price-closePrice);
 }
 double calcChangePercent(){
-    return ((price/closePrice)-1);
+    return ((price/closePrice)-1)*100.0 ;
 }
 void standardPrint(String ticker){
      lcd.setCursor(0,0);
@@ -76,11 +98,16 @@ void standardPrint(String ticker){
      lcd.print(" ");
      lcd.print(calcChange());
      lcd.print(" ");
+     /**
      if(calcChangePercent() < 0){
         lcd.print("-");
+     } else if (calcChangePercent() == 0){
+        lcd.print(" ");
      } else{
         lcd.print("+");
      }
+     **/
+     lcd.print("%");
      lcd.print(calcChangePercent());
      lcd.setCursor(0, 1);
      lcd.print(price);
--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 from yahoo_fin import stock_info as si
-import yfinance
 import serial
 from time import sleep
 
 def chooseTicker(count):
     if (ser.in_waiting):
-        ser.readLine()
-        if (ser == 'N'):
+        print(ser.read())      
+        if (ser.read() == b'\x00'):
             count+=1
             if (count == 9):
                 count = 0
-        elif (ser == 'B'):
+        elif (ser.read() == b'\x01'):
             count-=1
             if (count == -1):
                 count = 8
@@ -18,18 +17,18 @@ def getClose():
     for i in range(9):
         closePrice[i] = si.get_quote_table(tickers[i])['Previous Close']
 
-ser = serial.Serial('com10', 9600)
-count = 0;
+ser = serial.Serial('com5', 9600)
+count = 0
 tickers = ["gme", "amc", "bbby", "nok", "bb", 
            "tsla", "aapl", "dis", "nvda"]
 closePrice = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 getClose()
 
 while True:
-    chooseTicker(count);
+    chooseTicker(count)
     price = round(si.get_live_price(tickers[count]),2)
-    ser.write(str(chr(int(price))))
-    ser.write(str(chr(int((price%1)*100))))
-    ser.write(str(chr(int(closePrice[count]))))
-    ser.write(str(chr(int((closePrice[count]%1)*100))))
+    ser.write(int(price).to_bytes(1, 'big'))
+    ser.write(int((price%1)*100).to_bytes(1, 'big'))
+    ser.write(int(closePrice[count]).to_bytes(1, 'big'))
+    ser.write(int((closePrice[count]%1)*100).to_bytes(1, 'big'))
     sleep(0.1)

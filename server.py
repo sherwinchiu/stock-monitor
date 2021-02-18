22e0d4300bbf16516fec611f1f436ac6f27f7cc7
Sherwin Chiu <sherwin.chiu89@gmail.com>
2021-02-18 16:36:43 -0500

    hopefully done

--- server.py
+++ server.py
@@ -14,22 +14,22 @@ def chooseTicker(count):
             count-=1
             if (count == -1):
                 count = 8
-def getOpen():
+def getClose():
     for i in range(9):
-        openPrice[i] = si.get_quote_table(tickers[i])['Open']
+        closePrice[i] = si.get_quote_table(tickers[i])['Previous Close']
 
-#ser = serial.Serial('com10', 9600)
+ser = serial.Serial('com10', 9600)
 count = 0;
 tickers = ["gme", "amc", "bbby", "nok", "bb", 
            "tsla", "aapl", "dis", "nvda"]
-openPrice = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-getOpen()
-'''
+closePrice = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+getClose()
+
 while True:
     chooseTicker(count);
     price = round(si.get_live_price(tickers[count]),2)
-    print(price)
     ser.write(str(chr(int(price))))
     ser.write(str(chr(int((price%1)*100))))
+    ser.write(str(chr(int(closePrice[count]))))
+    ser.write(str(chr(int((closePrice[count]%1)*100))))
     sleep(0.1)
-    '''
